4dc889d0ee09d4cba25ec56c6d3792bc0185faec
bchanowski <bartosz.chanowski@gmail.com>
2025-08-25 19:36:24 +0200

    Fix for subgroup tab, should appear properly now

--- src/components/Categories/Category.tsx
+++ src/components/Categories/Category.tsx
@@ -13,31 +13,31 @@ const Category = ({ category, index }: Props) => {
   return (
     <>
       <div
-        className="cursor-pointer hidden h-full px-[4px] md:px-[2svw] py-[10px] sm:flex items-center ease-out duration-200 hover:text-sky-900 hover:bg-blue-100"
+        className="cursor-pointer relative hidden h-full px-[4px] md:px-[2svw] py-[10px] sm:flex items-center ease-out duration-200 hover:text-sky-900 hover:bg-blue-100"
         onMouseEnter={() => setShowOptions(true)}
         onMouseLeave={() => setShowOptions(false)}
       >
         {category.name}
         {category.options.hasOptions && <ChevronDown />}
+        {showOptions && category.options.hasOptions && (
+          <div
+            className={`absolute flex flex-wrap break-all bg-sky-50 top-[70px] w-[320px] max-w-[calc(100vw-2rem)] z-50  ${
+              index > categoriesList.length - 4 ? "right-0" : "left-0"
+            }`}
+          >
+            {category.options.option.map((sub, id) => (
+              <p
+                key={id}
+                className="inline-flex w-[50%] p-[1rem] break-all cursor-pointer ease-out duration-200 hover:bg-blue-100"
+              >
+                {sub}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
       {index < categoriesList.length - 1 && (
         <hr className="hidden sm:inline h-[80%] m-[0] border-r border-sky-900 border-solid" />
-      )}
-      {showOptions && category.options.hasOptions && (
-        <div
-          className="absolute flex flex-wrap break-all bg-sky-50 top-[198px] w-[320px]"
-          onMouseEnter={() => setShowOptions(true)}
-          onMouseLeave={() => setShowOptions(false)}
-        >
-          {category.options.option.map((sub, id) => (
-            <p
-              key={id}
-              className="inline-flex w-[50%] p-[1rem] break-all cursor-pointer ease-out duration-200 hover:text-sky-900 hover:bg-blue-100"
-            >
-              {sub}
-            </p>
-          ))}
-        </div>
       )}
     </>
   );
